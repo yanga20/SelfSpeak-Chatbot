66ecd2ace52286cd306f7ecd880c80359e1532a5
yanga20 <hayanga20@gmail.com>
2026-04-18 20:55:08 +0530

    Initial commit

---
+++ selfspeak.py
@@ -0,0 +1,59 @@
+import requests
+import asyncio
+import edge_tts
+import os
+
+PROJECT_PASSWORD = "selfspeak123"
+
+def authenticate():
+    password = input("Enter project password: ")
+
+    if password != PROJECT_PASSWORD:
+        print("Access denied.")
+        exit()
+
+    print("Access granted.\n")
+def play_my_voice():
+    print("Playing your voice sample...\n")
+    os.system("start Myvoice_sample.wav")
+
+async def speak(text):
+    communicate = edge_tts.Communicate(
+        text,
+        voice="en-IN-NeerjaNeural", 
+        rate="-10%",                  
+        pitch="+2Hz"                  
+    )
+    await communicate.save("output.mp3")
+    os.system("start output.mp3")
+
+def chat_with_ai(prompt):
+    url = "http://localhost:11434/api/generate"
+
+    data = {
+        "model": "phi",
+        "prompt": prompt,
+        "stream": False
+    }
+
+    response = requests.post(url, json=data)
+    return response.json()['response']
+
+authenticate()
+print("SelfSpeak AI Chatbot (type 'exit' to quit)\n")
+
+play_my_voice()
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    answer = chat_with_ai(user_input)
+
+    answer = "So, according to me, " + answer
+
+    print("AI:", answer)
+
+    asyncio.run(speak(answer))
